510f33217b4f31276418f98dab1d2f86979b2821
mu7affarmirzo <cmuzaffarmirzo@gmail.com>
2020-11-20 09:56:29 +0500

    added up to placket

--- shirtdesigner/admin.py
+++ shirtdesigner/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
-
+from .models import (
+    FabricModel,
+    StyleTypeModel,
+    SleeveTypeModel,
+    ShirtSleeveModel,
+    YokeTypeModel,
+    YokeModel,
+    ShoulderTypeModel,
+    ShoulderModel,
+    BackDetailsTypeModel,
+    BackDetailsModel,
+    BottomCutTypeModel,
+    BottomCutModel, CollarTypeModel,
+    CollarModel, CuffTypeModel, CuffModel, PlacketTypeModel, PlacketModel
+)
 # Register your models here.
+
+admin.site.register(FabricModel)
+admin.site.register(StyleTypeModel)
+admin.site.register(SleeveTypeModel)
+admin.site.register(ShirtSleeveModel)
+admin.site.register(YokeTypeModel)
+admin.site.register(YokeModel)
+admin.site.register(ShoulderTypeModel)
+admin.site.register(ShoulderModel)
+admin.site.register(BackDetailsTypeModel)
+admin.site.register(BackDetailsModel)
+admin.site.register(BottomCutTypeModel)
+admin.site.register(BottomCutModel)
+admin.site.register(CollarTypeModel)
+admin.site.register(CollarModel)
+admin.site.register(CuffTypeModel)
+admin.site.register(CuffModel)
+admin.site.register(PlacketTypeModel)
+admin.site.register(PlacketModel)
--- shirtdesigner/models.py
+++ shirtdesigner/models.py
@@ -18,3 +18,146 @@ class FabricModel(models.Model):
 
     def __str__(self):
         return self.fabric
+
+class StyleTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class SleeveTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class ShirtSleeveModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='sleeve', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(SleeveTypeModel,
+                               related_name='sleevetype', blank=True, null=True,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
+
+class YokeTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class YokeModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='yoke', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(YokeTypeModel,
+                               related_name='yoketype', blank=True, null=True,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
+
+class ShoulderTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class ShoulderModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='shoulder', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(ShoulderTypeModel,
+                               related_name='shouldertype', blank=True, null=True,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
+
+class BackDetailsTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class BackDetailsModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='backdetails', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(BackDetailsTypeModel,
+                              related_name='backtype', blank=True, null=True,
+                              on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
+
+class BottomCutTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class BottomCutModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='bottom', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(BottomCutTypeModel,
+                              related_name='bottomtype', blank=True, null=True,
+                              on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
+
+class CollarTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class CollarModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='collar', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(CollarTypeModel,
+                               related_name='collartype', blank=True, null=True,
+                               on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
+
+class CuffTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class CuffModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='cuff', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(CuffTypeModel,
+                               related_name='cufftype', blank=True, null=True,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
+
+class PlacketTypeModel(models.Model):
+    type = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.type
+
+class PlacketModel(models.Model):
+    fabric = models.ForeignKey(FabricModel,
+                              related_name='placket', blank=True, null=True,
+                              on_delete=models.CASCADE)
+    type = models.ForeignKey(PlacketTypeModel,
+                               related_name='plackettype', blank=True, null=True,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.fabric} {self.type}'
